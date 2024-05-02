520f079beda612013600268b863f0479c275b70b
hu-sawan <husseinsawan2003@gmail.com>
2024-05-02 15:14:55 +0300

    added authentication to the app

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -120,3 +122,22 @@ export const deleteInvoice = async (id: string) => {
 
   revalidatePath('/dashboard/invoices');
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return "Email/password doesn't match credentials.";
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
